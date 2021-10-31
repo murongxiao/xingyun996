3fb9030ca784f5cd4bb33d41bf35b1ede62d9f32
murongxiao <627375547@qq.com>
2021-10-31 18:17:23 +0800

    level 0 (js)

--- 第三次作业-level0-董潇-2021214102.js
+++ 第三次作业-level0-董潇-2021214102.js
@@ -1,17 +1,17 @@
 {
     let obj = new Object();
-    obj.name = 'duixiang1';
+    obj.name = 'dx1';
     obj.color = 'red';
     console.log(obj);
 } {
     let obj = {
-        name: 'duixiang2',
+        name: 'dx2',
         color: 'yellow'
     }
     console.log(obj);
 } {
     function Dx() {
-        this.name = 'duixiang3';
+        this.name = 'dx3';
         this.color = 'green';
     }
     let d = new Dx();
